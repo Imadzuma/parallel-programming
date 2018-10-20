534c8d1ace2a49052e51c1901dca782e5b53869e
Imadzuma <36950027+Imadzuma@users.noreply.github.com>
2018-10-20 18:40:34 +0300

    Add delete

--- 1608/mezina_md/main.cpp
+++ 1608/mezina_md/main.cpp
@@ -14,12 +14,12 @@ int main(int argc, char **argv)
 	int* v = NULL;
 	srand(time(0));
 
-	//Инициализация MPI
+	//Г€Г­ГЁГ¶ГЁГ Г«ГЁГ§Г Г¶ГЁГї MPI
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	//Определение и передача размеров вектора
+	//ГЋГЇГ°ГҐГ¤ГҐГ«ГҐГ­ГЁГҐ ГЁ ГЇГҐГ°ГҐГ¤Г Г·Г  Г°Г Г§Г¬ГҐГ°Г®Гў ГўГҐГЄГІГ®Г°Г 
 	if (rank == 0) {
 		std::cout << "I'm process " << rank << "! Please, enter vector size: ";
 		std::cin >> size_v;
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 	}
 	MPI_Bcast(&size_v, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	//Определение и передача вектора
+	//ГЋГЇГ°ГҐГ¤ГҐГ«ГҐГ­ГЁГҐ ГЁ ГЇГҐГ°ГҐГ¤Г Г·Г  ГўГҐГЄГІГ®Г°Г 
 	v = new int[size_v];
 	if (rank == 0) {
 		std::cout << "I'm process " << rank << "! I generate vector:";
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
 	}
 	MPI_Bcast(v, size_v, MPI_INT, 0, MPI_COMM_WORLD);
 
-	//Подсчет результата в процессах
+	//ГЏГ®Г¤Г±Г·ГҐГІ Г°ГҐГ§ГіГ«ГјГІГ ГІГ  Гў ГЇГ°Г®Г¶ГҐГ±Г±Г Гµ
 	int size_area = (size_v - 1) / num_proc + ((rank < (size_v - 1) % num_proc) ? 1 : 0);
 	int begin = ((size_v - 1) / num_proc) * rank + std::min((size_v - 1) % num_proc, rank);
 	int res_proc = 0;
@@ -47,12 +47,13 @@ int main(int argc, char **argv)
 		res_proc += ((v[begin + i] >= 0) != (v[begin + i + 1] >= 0)) ? 1 : 0;
 	std::cout << "I'm process " << rank << "! My area is " << begin << " to " << begin + size_area << ", my result is " << res_proc << std::endl;
 
-	//Подсчет общего результата
+	//ГЏГ®Г¤Г±Г·ГҐГІ Г®ГЎГ№ГҐГЈГ® Г°ГҐГ§ГіГ«ГјГІГ ГІГ 
 	int res;
 	MPI_Reduce(&res_proc, &res, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 	if (rank == 0) {
 		std::cout << "I'm process " << rank << "! We find result! It is " << res << std::endl;
 	}
+	delete[] v;
 	MPI_Finalize();
 	return 0;
 }
