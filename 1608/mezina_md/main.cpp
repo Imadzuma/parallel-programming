87e00b5d128fbb5bcd9df223e5ececb39fcccf39
katy1819 <katerinka.fedotova1819@gmail.com>
2018-11-01 10:32:39 +0300

    Finish version

--- 1608/mezina_md/main.cpp
+++ 1608/mezina_md/main.cpp
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 
 #include <algorithm>
 #include <cstdio>
@@ -12,6 +12,10 @@ int main(int argc, char **argv)
 	int rank, num_proc;
 	int size_v;
 	int* v = NULL;
+	double t;
+	int size_area;
+	int begin;
+	MPI_Status status;
 	srand(time(0));
 
 	//Èíèöèàëèçàöèÿ MPI
@@ -30,30 +34,40 @@ int main(int argc, char **argv)
 	//Îïðåäåëåíèå è ïåðåäà÷à âåêòîðà
 	v = new int[size_v];
 	if (rank == 0) {
-		std::cout << "I'm process " << rank << "! I generate vector:";
 		for (int i = 0; i < size_v; ++i) {
 			v[i] = rand() % 200 - 100;
-			std::cout << " " << v[i];
 		}
-		std::cout << std::endl;
+		if (size_v <= 20) {
+			std::cout << "I'm process " << rank << "! I generate vector:";
+			for (int i = 0; i < size_v; ++i) {
+				std::cout << " " << v[i];
+			}
+			std::cout << std::endl;
+		}
 	}
 	MPI_Bcast(v, size_v, MPI_INT, 0, MPI_COMM_WORLD);
 
 	//Ïîäñ÷åò ðåçóëüòàòà â ïðîöåññàõ
-	int size_area = (size_v - 1) / num_proc + ((rank < (size_v - 1) % num_proc) ? 1 : 0);
-	int begin = ((size_v - 1) / num_proc) * rank + std::min((size_v - 1) % num_proc, rank);
+	if (rank == 0) 
+		t = MPI_Wtime();
+	size_area = (size_v - 1) / num_proc + ((rank < (size_v - 1) % num_proc) ? 1 : 0);
+	begin = ((size_v - 1) / num_proc) * rank + std::min((size_v - 1) % num_proc, rank);
 	int res_proc = 0;
 	for (int i = 0; i < size_area; ++i)
 		res_proc += ((v[begin + i] >= 0) != (v[begin + i + 1] >= 0)) ? 1 : 0;
-	std::cout << "I'm process " << rank << "! My area is " << begin << " to " << begin + size_area << ", my result is " << res_proc << std::endl;
+	if (size_area!=0)
+		std::cout << "I'm process " << rank << "! My area is " << begin << " to " << begin + size_area << ", my result is " << res_proc << std::endl;
+	else
+		std::cout << "I'm process " << rank << "! I don't have any elements. My result is " << res_proc << std::endl;
 
 	//Ïîäñ÷åò îáùåãî ðåçóëüòàòà
-	int res;
+	int res = 0;
 	MPI_Reduce(&res_proc, &res, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 	if (rank == 0) {
-		std::cout << "I'm process " << rank << "! We find result! It is " << res << std::endl;
+		std::cout << "I'm process " << rank << "! We find result! It is " << res << "! Time is "<<MPI_Wtime() - t<< std::endl;
 	}
-	delete[] v;
+	if (v!=NULL)
+		delete[] v;
 	MPI_Finalize();
 	return 0;
 }
